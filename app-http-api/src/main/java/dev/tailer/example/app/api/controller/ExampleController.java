3e9429d378c3e0a4f7fffb269db4387f0a864953
Tailer <captaintailer@gmail.com>
2024-01-13 01:19:38 +0900

    스프링 Doc 설정 추가

--- app-http-api/src/main/java/dev/tailer/example/app/api/controller/ExampleController.java
+++ app-http-api/src/main/java/dev/tailer/example/app/api/controller/ExampleController.java
@@ -1,5 +1,6 @@
 package dev.tailer.example.app.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import java.util.Random;
 @Slf4j
 @RestController
 public class ExampleController {
+
+    @Operation(summary = "테스트", description = "demo 조회 메서드입니다.")
     @GetMapping(path = "/test")
     public ResponseEntity<String> findOrganizationUserList() {
         return ResponseEntity.ok(String.valueOf(new Random().nextInt()));
